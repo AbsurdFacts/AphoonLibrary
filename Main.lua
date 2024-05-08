cbf5c102e7f5682c43d786c1343afe86d382d57d
AbsurdFacts <163673134+AbsurdFacts@users.noreply.github.com>
2024-05-08 18:36:03 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,135 +1,137 @@
-local Players = game:GetService("Players")
-local HttpService = game:GetService("HttpService")
+local TabFrame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local CombatText = Instance.new("TextLabel")
+local uiStroke = Instance.new("UIStroke")
 
-local function CreateTab(params)
-    local playerGui = Players.LocalPlayer:WaitForChild("PlayerGui")
-    local screenGui = Instance.new("ScreenGui")
-    screenGui.Parent = playerGui
+local LogoText = Instance.new("TextLabel")
+local UIGradient = Instance.new("UIGradient")
 
-    local tabFrame = Instance.new("Frame")
-    tabFrame.Name = params.TABNAME
-    tabFrame.Position = params.POSITION
-    tabFrame.Size = params.SIZE or UDim2.new(0, 200, 0, 50)
-    tabFrame.BackgroundTransparency = 0.5
+function CreateTab(tabname, position)
+	TabFrame.Name = tabname .. "Frame"
+	TabFrame.Parent = game.StarterGui.Oxygen
+	TabFrame.BackgroundColor3 = Color3.fromRGB(209, 208, 210)
+	TabFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TabFrame.BorderSizePixel = 0
+	TabFrame.Position = position
+	TabFrame.Size = UDim2.new(0.20181635, 0, 0.91390729, 0)
 
-    local gradient = Instance.new("UIGradient")
-    gradient.Rotation = 90
-    gradient.Color = ColorSequence.new{
-        ColorSequenceKeypoint.new(0, Color3.fromRGB(40, 40, 40)),
-        ColorSequenceKeypoint.new(0.5, Color3.fromRGB(30, 30, 60)),
-        ColorSequenceKeypoint.new(1, Color3.fromRGB(30, 30, 70))
-    }
-    gradient.Parent = tabFrame
+	UICorner.Parent = TabFrame
 
-    local uiStroke = Instance.new("UIStroke")
-    uiStroke.Thickness = 2
-    uiStroke.Color = Color3.new(0, 0, 0)
-    uiStroke.Parent = tabFrame
+	CombatText.Name = "CombatText"
+	CombatText.Parent = TabFrame
+	CombatText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	CombatText.BackgroundTransparency = 1.000
+	CombatText.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	CombatText.BorderSizePixel = 0
+	CombatText.Position = TabFrame.Position
+	CombatText.Size = UDim2.new(0.675000012, 0, 0.09710145, 0)
+	CombatText.Font = Enum.Font.Ubuntu
+	CombatText.Text = tabname
+	CombatText.TextColor3 = Color3.fromRGB(0, 0, 0)
+	CombatText.TextScaled = true
+	CombatText.TextSize = 14.000
+	CombatText.TextWrapped = true
+	UICorner.Parent = TabFrame
 
-    local uiCorner = Instance.new("UICorner")
-    uiCorner.CornerRadius = UDim.new(0, 8)
-    uiCorner.Parent = tabFrame
-
-    local titleLabel = Instance.new("TextLabel")
-    titleLabel.Name = "TitleLabel"
-    titleLabel.Parent = tabFrame
-    titleLabel.Size = UDim2.new(1, 0, 0, 20)
-    titleLabel.Position = UDim2.new(0, 0, 0, 5)
-    titleLabel.Text = params.NAME
-    titleLabel.TextSize = 32
-    titleLabel.Font = Enum.Font.DenkOne
-    titleLabel.TextColor3 = Color3.new(1, 1, 1)
-    titleLabel.TextStrokeTransparency = 0
-    titleLabel.TextStrokeColor3 = Color3.new(0, 0, 0)
-    titleLabel.BackgroundTransparency = 1
-
-    tabFrame.Parent = screenGui
-
-    return tabFrame
+	uiStroke.Thickness = 2
+	uiStroke.Color = Color3.new(0, 0, 0)
+	uiStroke.Parent = TabFrame
+	
+	return TabFrame
 end
+function CreateButton(buttonname, parent, params, toggleStates, callback)
+	local Button = Instance.new("TextButton")
+	Button.Name = "Button"
+	Button.Parent = game.StarterGui.Oxygen.HackFrame.VisualFrame
+	Button.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Button.BackgroundTransparency = 1.000
+	Button.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Button.BorderSizePixel = 0
+	Button.Position = UDim2.new(0.25, 0, 0.171014488, 0)
+	Button.Size = UDim2.new(0.5, 0, 0.0724637657, 0)
+	Button.Font = Enum.Font.SourceSans
+	Button.TextColor3 = Color3.fromRGB(0, 0, 0)
+	Button.TextScaled = true
+	Button.TextSize = 14.000
+	Button.TextWrapped = true
+	local isToggled = toggleStates[params.Name] or params.DefaultValue or false
 
-local function CreateToggle(params)
-    local toggleButton = Instance.new("TextButton")
-    toggleButton.Name = params.Name
-    toggleButton.Parent = params.ParentTab
-    toggleButton.Position = params.Position or UDim2.new(0, 0, 0, 0)
-    toggleButton.Size = UDim2.new(0, 200, 0, 30)
-    toggleButton.BackgroundTransparency = 1
-    toggleButton.Text = params.Name
-    toggleButton.TextSize = 24
-    toggleButton.Font = Enum.Font.DenkOne
-    toggleButton.TextColor3 = Color3.new(1, 1, 1)
-    toggleButton.TextStrokeTransparency = 1
+	Button.MouseButton1Click:Connect(function()
+		isToggled = not isToggled
+		params.Callback(isToggled)
 
-    local filePath = "ToggleStates.json"
-    local toggleStates = {}
+		Button.TextColor3 = isToggled and Color3.new(0.5, 0, 0.8) or Color3.new(1, 1, 1)
+		Button.BackgroundColor3 = isToggled and Color3.new(0.2, 0.2, 0.2) or Color3.new(0, 0, 0)
 
-    local fileExists, fileContent = pcall(function()
-        return game:GetService("HttpService"):JSONDecode(readfile(filePath))
-    end)
-
-    if fileExists and fileContent then
-        toggleStates = fileContent
-    end
-
-    local isToggled = toggleStates[params.Name] or params.DefaultValue or false
-
-    toggleButton.MouseButton1Click:Connect(function()
-        isToggled = not isToggled
-        params.Callback(isToggled)
-
-        toggleButton.TextColor3 = isToggled and Color3.new(0.5, 0, 0.8) or Color3.new(1, 1, 1)
-        toggleButton.BackgroundColor3 = isToggled and Color3.new(0.2, 0.2, 0.2) or Color3.new(0, 0, 0)
-
-        toggleStates[params.Name] = isToggled
-        writefile(filePath, HttpService:JSONEncode(toggleStates))
-    end)
-
-    return toggleButton
+		toggleStates[params.Name] = isToggled
+	end)
+	
+	return Button
 end
+function CreateLogo(text)
+	LogoText.Name = text
+	LogoText.Parent = game.StarterGui.Oxygen
+	LogoText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	LogoText.BackgroundTransparency = 1.000
+	LogoText.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	LogoText.BorderSizePixel = 0
+	LogoText.Size = UDim2.new(0.101112232, 0, 0.0662251636, 0)
+	LogoText.Font = Enum.Font.Ubuntu
+	LogoText.Text = text
+	LogoText.TextColor3 = Color3.fromRGB(0, 0, 0)
+	LogoText.TextScaled = true
+	LogoText.TextSize = 14.000
+	LogoText.TextWrapped = true
 
-local function CreateTabToggle(params)
-    local screenGui = Instance.new("ScreenGui")
-    screenGui.Parent = Players.LocalPlayer.PlayerGui
+	UIGradient.Transparency = NumberSequence.new{NumberSequenceKeypoint.new(0.00, 0.00), NumberSequenceKeypoint.new(0.22, 0.62), NumberSequenceKeypoint.new(0.68, 0.85), NumberSequenceKeypoint.new(1.00, 0.00)}
+	UIGradient.Parent = LogoText
+	
+	return LogoText
+end
+function CreateTabToggle(params, text)
+	local player = game.Players.LocalPlayer
+	local screenGui = Instance.new("ScreenGui")
+	screenGui.Parent = player.PlayerGui
 
-    local toggleButton = Instance.new("TextButton")
-    toggleButton.Name = "TabToggle"
-    toggleButton.Parent = screenGui
-    toggleButton.AnchorPoint = Vector2.new(1, 0.5)
-    toggleButton.Position = params.Position or UDim2.new(0.95, 0, 0.4, 0)
-    toggleButton.Size = params.Size or UDim2.new(0, 80, 0, 40)
-    toggleButton.BackgroundTransparency = 0.5
-    toggleButton.BackgroundColor3 = Color3.new(0, 0, 0)
-    toggleButton.Text = "Toggle"
-    toggleButton.TextSize = 18
-    toggleButton.Font = Enum.Font.DenkOne
-    toggleButton.TextColor3 = Color3.new(1, 1, 1)
+	local toggleButton = Instance.new("TextButton")
+	toggleButton.Name = "TabToggle"
+	toggleButton.Parent = screenGui
+	toggleButton.AnchorPoint = Vector2.new(1, 0.5)
+	toggleButton.Position = params.Position or UDim2.new(0.95, 0, 0.4, 0)
+	toggleButton.Size = params.Size or UDim2.new(0, 80, 0, 40)
+	toggleButton.BackgroundTransparency = 0.5
+	toggleButton.BackgroundColor3 = Color3.new(0, 0, 0)
+	toggleButton.Text = text
+	toggleButton.TextSize = 18
+	toggleButton.Font = Enum.Font.DenkOne
+	toggleButton.TextColor3 = Color3.new(1, 1, 1)
 
-    local uiStroke = Instance.new("UIStroke")
-    uiStroke.Thickness = 1
-    uiStroke.Color = Color3.new(0, 0, 0)
-    uiStroke.Parent = toggleButton
+	local uiStroke = Instance.new("UIStroke")
+	uiStroke.Thickness = 1
+	uiStroke.Color = Color3.new(0, 0, 0)
+	uiStroke.Parent = toggleButton
 
-    local uiCorner = Instance.new("UICorner")
-    uiCorner.CornerRadius = UDim.new(0, 8)
-    uiCorner.Parent = toggleButton
+	local uiCorner = Instance.new("UICorner")
+	uiCorner.CornerRadius = UDim.new(0, 8)
+	uiCorner.Parent = toggleButton
 
-    local tabsEnabled = true  
+	local tabsEnabled = true  
 
-    toggleButton.MouseButton1Click:Connect(function()
-        tabsEnabled = not tabsEnabled
-        params.Callback(tabsEnabled)
+	toggleButton.MouseButton1Click:Connect(function()
+		tabsEnabled = not tabsEnabled
+		params.Callback(tabsEnabled)
 
-        toggleButton.TextColor3 = tabsEnabled and Color3.new(1, 1, 1) or Color3.new(0.5, 0, 0.8)
-    end)
+		toggleButton.TextColor3 = tabsEnabled and Color3.new(1, 1, 1) or Color3.new(0.5, 0, 0.8)
+	end)
 
-    toggleButton.Visible = true  
+	toggleButton.Visible = true  
 
-    return toggleButton
+	return toggleButton
 end
 
 return {
     CreateTab = CreateTab,
-    CreateToggle = CreateToggle,
+    CreateButton = CreateButton,
+    CreateLogo = CreateLogo,
     CreateTabToggle = CreateTabToggle
 }
